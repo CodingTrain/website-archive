de4d61adcd0bd43ec414c91055776e8060d30f46
EmbraceLife <1227561934@qq.com>
2015-10-17 07:34:04 +0800

    1.2 p5.js Color delete starting version of code example, keeping the finished code example

--- code/1.2_p5.js_Color/sketch.js
+++ code/1.2_p5.js_Color/sketch.js
@@ -1,25 +1,30 @@
 /*
 code from video: https://youtu.be/9mucjcrhFcM?list=PLRqwX-V7Uu6Zy51Q-x9tMWIv9cueOFTFA
-code can be seen from 7:32 second
+code can be seen from 9:47 second onward
 */
 
 
 function setup() {
-  createCanvas(600, 300);
+  createCanvas(600, 400);  
 }
 
+
 function draw() {
-  background(255);
+  //background(255);
+  background(150, 150, 200);
   ellipseMode(CENTER);
   rectMode(CENTER);
   
   // Body
+  fill(255, 0, 0);
   rect(240, 145, 20, 100);
   
-  // Head
+  // Head 
+  fill(0, 0, 255);
   ellipse(240, 115, 60, 60);
   
   // Eyes
+  fill(0, 255, 0);
   ellipse(221, 115, 16, 32);
   ellipse(259, 115, 16, 32);
   
