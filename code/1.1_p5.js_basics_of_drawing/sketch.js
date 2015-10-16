13683724d61d4fe72b6b9a0c5dd87bd8c5c7c868
EmbraceLife <1227561934@qq.com>
2015-10-16 22:52:33 +0800

    add https address and exact time of the code in video

--- code/1.1_p5.js_basics_of_drawing/sketch.js
+++ code/1.1_p5.js_basics_of_drawing/sketch.js
@@ -1,3 +1,9 @@
+/*
+1. Code below is for https://youtu.be/D1ELEeIs0j8?list=PLRqwX-V7Uu6Zy51Q-x9tMWIv9cueOFTFA
+2. Code is at 7:57 seconds on drawing rectangle
+*/
+
+
 function setup() {
   createCanvas(640, 360);
 }
