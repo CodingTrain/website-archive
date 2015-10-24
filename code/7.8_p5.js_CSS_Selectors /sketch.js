b2138f8279e5fe3c734274424afc6851a1f57610
EmbraceLife <1227561934@qq.com>
2015-10-24 19:23:37 +0800

    7.8 CSS selectors

---
+++ code/7.8_p5.js_CSS_Selectors /sketch.js
@@ -0,0 +1,14 @@
+// https://vimeo.com/channels/learningp5js/143374873
+
+// p5 DOM reference: p5js.org/reference/#/libraries/p5.dom
+
+// All examples: github.com/shiffman/Video-Lesson-Materials
+
+
+function setup() {
+  
+}
+
+function draw() {
+  
+}
