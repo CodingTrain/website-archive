eaa8f5d2fb09330458ae50ddedbbef2907865076
Daniel Shiffman <daniel@shiffman.net>
2016-03-28 10:22:39 -0400

    code for 8.6

---
+++ code_p5.js/8.6_p5.js_api_query_user_input/sketch.js
@@ -0,0 +1,35 @@
+var weather;
+
+var api = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var apiKey = '&APPID=001b0f58045147663b1ea518d34d88b4';
+var units = '&units=metric';
+
+var input;
+
+function setup() {
+  createCanvas(400, 200);
+
+  var button = select('#submit');
+  button.mousePressed(weatherAsk);
+
+  input = select('#city');
+}
+
+function weatherAsk() {
+  var url = api + input.value() + apiKey + units;
+  loadJSON(url, gotData);
+}
+
+function gotData(data) {
+  weather = data;
+}
+
+function draw() {
+  background(0);
+  if (weather) {
+    var temp = weather.main.temp;
+    var humidity = weather.main.humidity;
+    ellipse(100, 100, temp, temp);
+    ellipse(300, 100, humidity, humidity);
+  }
+}
