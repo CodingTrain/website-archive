d0c897965ee6130ba56777b89f56a984208a375f
Daniel Shiffman <daniel@shiffman.net>
2016-07-15 15:28:15 -0400

    Q&A playlist

---
+++ Q&A/Q_2_WEGBGL/sketch.js
@@ -0,0 +1,38 @@
+// Daniel Shiffman
+// http://codingrainbow.com
+// http://patreon.com/codingrainbow
+// Code for: https://youtu.be/BrFZ5RkywcY
+
+var angle = 0;
+
+var img;
+
+function preload() {
+  img = loadImage("hedge.jpg");
+  l
+}
+
+function setup() {
+  createCanvas(400, 400, WEBGL);
+}
+
+function draw() {
+  background(51);
+  pointLight(255, 0, 255, 200, 0, 0);
+  push();
+  translate(200, 0, 0);
+  sphere(10);
+  pop();
+  translate(0, 0, mouseX);
+  rotateX(angle);
+  rotateY(angle * 0.6);
+  texture(img);
+  box(50);
+
+  angle += 0.02;
+
+  //fill(0, 255, 0);
+  //rect(200, 200, 100, 100);
+
+  //ellipse(200, 200, 100, 100);
+}
