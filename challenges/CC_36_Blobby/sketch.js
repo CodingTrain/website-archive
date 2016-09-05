d9c4ba931f4a26dfe86e7bd360c398e180a03c40
Daniel Shiffman <daniel@shiffman.net>
2016-09-05 08:58:29 -0700

    blobbly

---
+++ challenges/CC_36_Blobby/sketch.js
@@ -0,0 +1,33 @@
+// Daniel Shiffman
+// http://codingrainbow.com
+// http://patreon.com/codingrainbow
+// Code for this video: https://youtu.be/Cl_Gjj80gPE
+
+var yoff = 0.0;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+
+  translate(width / 2, height / 2);
+
+  var radius = 150;
+
+  beginShape();
+  var xoff = 0;
+  for (var a = 0; a < TWO_PI; a += 0.1) {
+    var offset = map(noise(xoff, yoff), 0, 1, -25, 25);
+    var r = radius + offset;
+    var x = r * cos(a);
+    var y = r * sin(a);
+    vertex(x, y);
+    xoff += 0.1;
+    //ellipse(x, y, 4, 4);
+  }
+  endShape();
+
+  yoff += 0.01;
+}
