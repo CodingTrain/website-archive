708989290747c9768731869e396b5106179f0508
Daniel Shiffman <daniel@shiffman.net>
2016-09-24 16:32:08 -0400

    coding challenge code

---
+++ challenges/CC_38_word_interactor/sketch.js
@@ -0,0 +1,46 @@
+// Daniel Shiffman
+// http://codingrainbow.com
+// http://patreon.com/codingrainbow
+// Code for: https://youtu.be/AKuW48WeNMA
+
+var textfield;
+var output;
+var submit;
+
+function setup() {
+  console.log(this);
+  noCanvas();
+  textfield = select("#input");
+  output = select('#output');
+  submit = select("#submit");
+  submit.mousePressed(newText);
+}
+
+function highlight() {
+  console.log(this.html());
+  this.html('rainbow');
+  var c = color(random(255), random(255), random(255));
+  this.style('background-color', c);
+  //var s = this.html().replace(
+
+  //console.log(this);
+  //console.log('hover');
+}
+
+function newText() {
+  var s = textfield.value();
+
+  var words = s.split(/(\W+)/);
+  console.log(words);
+  for (var i = 0; i < words.length; i++) {
+
+    var span = createSpan(words[i]);
+    span.parent(output);
+
+    if (!/\W+/.test(words[i])) {
+      //span.style('background-color', color(random(255), 0, random(255)));
+      span.mouseOver(highlight);
+    }
+  }
+  //console.log(words);
+}
---
+++ challenges/CC_39_madlibs/sketch.js
@@ -0,0 +1,36 @@
+// Daniel Shiffman
+// http://codingrainbow.com
+// http://patreon.com/codingrainbow
+// Code for: https://youtu.be/ziBO-U2_t3k
+
+var data;
+
+var txt = '$$Exclamation$$! they said $$Adverb$$ as they jumped into their $$Noun$$ and flew off with their $$Adjective$$ $$PluralNoun$$.';
+
+function setup() {
+  noCanvas();
+  Tabletop.init({
+    key: '15WyEmfu6B1UCzzqeacYnzI8lutrxF6uWvFDiSteBqTs',
+    callback: gotData,
+    simpleSheet: true
+  });
+
+  var button = createButton('generate madlib');
+  button.mousePressed(generate);
+}
+
+function replacer(match, pos) {
+  var entry = random(data);
+  return entry[pos];
+}
+
+
+function generate() {
+  //console.log('generate');
+  var madlib = txt.replace(/\$\$(.*?)\$\$/g, replacer);
+  createP(madlib);
+}
+
+function gotData(stuff, tabletop) {
+  data = stuff;
+}
