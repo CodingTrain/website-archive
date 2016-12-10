f782bca7f8727cdb025161ceb1a8ca88a7f6ee39
blinkityblank <mathieu.blanchette@gmail.com>
2016-12-10 15:17:25 -0500

    Added Session 2 and removed Coding Challenges

---
+++ A2Z/README.md
@@ -0,0 +1,10 @@
+# Session 2: Regular Expressions - Programming from A to Z
+
+* Youtube Playlist: https://www.youtube.com/watch?v=7DG3kCDx53c&index=1&list=PLRqwX-V7Uu6YEypLuls7iidwHMdCM6o2w
+
+Link to source code to Coding Challenge #38: Word Interactor:
+
+* https://github.com/CodingRainbow/Rainbow-Code/tree/master/challenges/CC_38_word_interactor
+
+# Questions? Need Support?
+* Join: http://patreon.com/codingrainbow
---
+++ A2Z/session2/2-08_regexSplit/sketch.js
@@ -0,0 +1,28 @@
+// Daniel Shiffman
+// http://codingrainbow.com
+// http://patreon.com/codingrainbow
+// Code for: https://youtu.be/fdyqutmcI2Q
+
+
+var textfield;
+var output;
+var submit;
+
+function setup() {
+  noCanvas();
+  textfield = select("#input");
+  //textfield.changed(newText);
+  output = select('#output');
+  submit = select("#submit");
+  submit.mousePressed(newText);
+}
+
+function newText() {
+  var s = textfield.value();
+  var r = /(\W+)/;
+  var words = s.split(r);
+  console.log(words);
+  for (var i = 0; i < words.length; i++) {
+    createP(words[i]);
+  }
+}
