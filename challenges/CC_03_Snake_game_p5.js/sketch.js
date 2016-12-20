f7cdd99f8368b677b7ed8526d4e56a062fe576b1
lucky70707 <zoetepopcorn@live.nl>
2016-12-20 17:54:31 +0100

    Update sketch.js
    added code to start the snake as two blocks instead of one. Added code to check whether the generation point of a new food is in the same place as the tail of the snake.

--- challenges/CC_03_Snake_game_p5.js/sketch.js
+++ challenges/CC_03_Snake_game_p5.js/sketch.js
@@ -4,56 +4,64 @@
 // Code for: https://youtu.be/AaGK-fj-BAM
 
 var s;
-var scl = 20;
+var scl = 50;
 
 var food;
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(600,600);
   s = new Snake();
-  frameRate(10);
+  frameRate(8);
   pickLocation();
-
+  s.start();
 }
 
-function pickLocation() {
+
+
+function pickLocation(){
   var cols = floor(width/scl);
   var rows = floor(height/scl);
-  food = createVector(floor(random(cols)), floor(random(rows)));
-  food.mult(scl);
-}
+  var isPossibleFoodPlacement = false;
 
-function mousePressed() {
-  s.total++;
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  console.log("x van food is ",food.x);
+
+  var isPossibleFoodPlacement = !(s.checkForTail(food));//als er iets gevonden is komt er een true uit maar dan moet isPossibleFoodPlacement false zijn
+
+  while (isPossibleFoodPlacement ===false) {
+    food = createVector(floor(random(cols)), floor(random(rows)));
+    var isPossibleFoodPlacement = !(s.checkForTail(food));//als er iets gevonden is komt er een true uit maar dan moet isPossibleFoodPlacement false zij
+  }
+  var isPossibleFoodPlacement = false;
+
+  food.mult(scl);
 }
 
 function draw() {
   background(51);
 
-  if (s.eat(food)) {
+
+
+  if(s.eat(food)){
     pickLocation();
   }
   s.death();
   s.update();
   s.show();
 
-
-  fill(255, 0, 100);
+  fill(255,0,100);
   rect(food.x, food.y, scl, scl);
 }
 
 
+function keyPressed(){
 
-
-
-function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    s.dir(0, -1);
-  } else if (keyCode === DOWN_ARROW) {
-    s.dir(0, 1);
-  } else if (keyCode === RIGHT_ARROW) {
-    s.dir(1, 0);
-  } else if (keyCode === LEFT_ARROW) {
-    s.dir(-1, 0);
-  }
+  if (keyCode === UP_ARROW){
+    s.dir(0,-1);
+  }else if (keyCode === DOWN_ARROW) {
+    s.dir(0,1);
+  }else if (keyCode === RIGHT_ARROW) {
+      s.dir(1,0);}
+      else if (keyCode === LEFT_ARROW) {
+        s.dir(-1,0);}
 }
