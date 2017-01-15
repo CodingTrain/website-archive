578d6f709566fc3217682a45e70ff63deb5aad54
Daniel Shiffman <daniel@shiffman.net>
2017-01-15 16:01:29 -0500

    video links

--- challenges/CC_51_astar/sketch.js
+++ challenges/CC_51_astar/sketch.js
@@ -1,7 +1,9 @@
 // Daniel Shiffman
 // http://codingtra.in
 // http://patreon.com/codingtrain
-// Code for: [ADD WHEN UPLOADED]
+// Part 1: https://youtu.be/aKYlikFAV4k
+// Part 2: https://youtu.be/EaZxUCWAjb0
+// Part 3: https://youtu.be/jwRT4PCT6RU
 
 // Function to delete element from the array
 function removeFromArray(arr, elt) {
