5531146f9e7cc9763fe539241f81bf6cb116e98c
Daniel Shiffman <daniel@shiffman.net>
2017-02-01 12:07:20 -0500

    adding random walks and fixing numbering

---
+++ challenges/CC_53_random_walk_levy/sketch.js
@@ -0,0 +1,38 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Video: https://youtu.be/bqF9w9TTfeo
+
+// var x;
+// var y;
+var pos;
+var prev;
+
+function setup() {
+  createCanvas(400, 400);
+  background(51);
+  pos = createVector(200, 200);
+  prev = pos.copy();
+  console.log(pos);
+}
+
+function draw() {
+  stroke(255);
+  strokeWeight(2);
+  //point(pos.x, pos.y);
+  line(pos.x, pos.y, prev.x, prev.y);
+  prev.set(pos);
+
+  var step = p5.Vector.random2D();
+
+  var r = random(100);
+  if (r < 1) {
+    step.mult(random(25, 100));
+  } else {
+    step.setMag(2);
+  }
+
+  //pos = pos + step;
+  pos.add(step);
+
+}
