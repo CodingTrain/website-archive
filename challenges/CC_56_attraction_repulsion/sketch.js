dc150640f86d279f484dc94d1749bd61f82800ec
Daniel Shiffman <daniel@shiffman.net>
2017-02-09 11:46:50 -0500

    attraction challenge

---
+++ challenges/CC_56_attraction_repulsion/sketch.js
@@ -0,0 +1,44 @@
+// Daniel Shiffman
+// http://codingtra.in
+// Attraction / Repulsion
+// Video: https://youtu.be/OAcXnzRNiCY
+
+var attractors = [];
+var particles = [];
+
+function setup() {
+  createCanvas(400, 400);
+
+  // for (var i = 0; i < 10; i++) {
+  //   attractors.push(createVector(random(width), random(height)));
+  // }
+}
+
+function mousePressed() {
+  attractors.push(createVector(mouseX, mouseY));
+}
+
+function draw() {
+  background(51);
+  stroke(255);
+  strokeWeight(4);
+  particles.push(new Particle(random(width), random(height)));
+
+  if (particles.length > 100) {
+    particles.splice(0, 1);
+  }
+
+  for (var i = 0; i < attractors.length; i++) {
+    stroke(0, 255, 0);
+    point(attractors[i].x, attractors[i].y);
+  }
+  for (var i = 0; i < particles.length; i++) {
+    var particle = particles[i];
+    for (var j = 0; j < attractors.length; j++) {
+      particle.attracted(attractors[j]);
+    }
+    particle.update();
+    particle.show();
+  }
+
+}
