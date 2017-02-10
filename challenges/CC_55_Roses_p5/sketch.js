5c0289fa5784bddffe164ee88a46ad900907b925
Ståle Raknes <stale.raknes@knowit.no>
2017-02-10 15:22:01 +0100

    Removing decimal from input slider and boosted the range Adding reduce denominator function for the loop Only trigger draw when input data is changed

--- challenges/CC_55_Roses_p5/sketch.js
+++ challenges/CC_55_Roses_p5/sketch.js
@@ -11,8 +11,10 @@ var sliderN;
 
 function setup() {
   createCanvas(400, 400);
-  sliderD = createSlider(1, 10, 5, 0.1);
-  sliderN = createSlider(1, 10, 5, 0.1);
+  sliderD = createSlider(1, 20, 10, 1);
+  sliderN = createSlider(1, 20, 10, 1);
+  sliderD.elt.addEventListener("input",draw);
+  sliderN.elt.addEventListener("input",draw);
 }
 
 function draw() {
@@ -20,19 +22,27 @@ function draw() {
   n = sliderN.value();
   var k = n / d;
   background(51);
+  push();
   translate(width / 2, height / 2);
 
   beginShape();
   stroke(255);
   noFill();
   strokeWeight(1);
-  for (var a = 0; a < TWO_PI * d; a += 0.02) {
+  for (var a = 0; a < TWO_PI * reduceDenominator(n, d); a += 0.02) {
     var r = 200 * cos(k * a);
     var x = r * cos(a);
     var y = r * sin(a);
     vertex(x, y);
   }
   endShape(CLOSE);
+  pop();
+  noLoop();
+}
 
-
+function reduceDenominator(numerator, denominator) {
+    function rec(a, b) {
+        return b ? rec(b, a % b) : a;
+    }
+    return denominator / rec(numerator, denominator);
 }
