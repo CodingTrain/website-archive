3f4306fa1de15578f3ca6b5a84de7d583e857786
SacrificeProductions <the_gaming_monkey@hotmail.co.uk>
2017-02-18 00:03:33 +0000

    Update sketch.js
    Fix for using TRIANGLE_STRIP not working properly with p5, the need for specify a fill and using opacity to help visual aid as have not looked into shading.

--- challenges/CC_11_PerlinNoiseTerrain_p5.js/sketch.js
+++ challenges/CC_11_PerlinNoiseTerrain_p5.js/sketch.js
@@ -3,6 +3,8 @@
 // http://patreon.com/codingtrain
 // Code for: https://youtu.be/IKB1hWWedMk
 
+// Edited by SacrificeProductions
+
 var cols, rows;
 var scl = 20;
 var w = 1400;
@@ -10,16 +12,18 @@ var h = 1000;
 
 var flying = 0;
 
-var terrain;
+var terrain = [];
 
 function setup() {
   createCanvas(600, 600, WEBGL);
   cols = w / scl;
   rows = h/ scl;
 
-  terrain = [];
   for (var x = 0; x < cols; x++) {
     terrain[x] = [];
+    for (var y = 0; y < cols; y++) {
+      terrain[x][y] = 0; //specify a default value for now
+    }
   }
 }
 
@@ -40,17 +44,14 @@ function draw() {
   background(0);
   translate(0, 50);
   rotateX(-PI/3);
+  fill(200,200,200, 50);
   translate(-w/2, -h/2);
   for (var y = 0; y < rows-1; y++) {
-    //beginShape(TRIANGLE_STRIP);
+    beginShape(TRIANGLE_STRIP);
     for (var x = 0; x < cols; x++) {
-      push();
-      translate(x*scl, y*scl, terrain[x][y]);
-      box(scl);
-      pop();
-      //vertex(x*scl, y*scl, terrain[x][y]);
-      //vertex(x*scl, (y+1)*scl, terrain[x][y+1]);
+      vertex(x*scl, y*scl, terrain[x][y]);
+      vertex(x*scl, (y+1)*scl, terrain[x][y+1]);
     }
-    //endShape();
+    endShape();
   }
 }
