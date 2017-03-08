81de91213ed5481831afceb19fc81f59d8c68a86
Nikola <markovicNRT@gmail.com>
2017-03-08 10:46:48 +0100

    Update particle.js

--- challenges/CC_27_Fireworks_2D_p5/particle.js
+++ challenges/CC_27_Fireworks_2D_p5/particle.js
@@ -1,17 +1,22 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/CKeyIbT3vXI
+
 function Particle(x, y, hu, firework) {
   this.pos = createVector(x, y);
   this.firework = firework;
   this.lifespan = 255;
   this.hu = hu;
-
+  this.acc = createVector(0, 0);
+  
   if (this.firework) {
     this.vel = createVector(0, random(-12, -8));
   } else {
     this.vel = p5.Vector.random2D();
     this.vel.mult(random(2, 10));
   }
-  this.acc = createVector(0, 0);
-
+ 
   this.applyForce = function(force) {
     this.acc.add(force);
   }
@@ -21,11 +26,10 @@ function Particle(x, y, hu, firework) {
       this.vel.mult(0.9);
       this.lifespan -= 4;
     }
+    
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.mult(0);
-
-
   }
 
   this.done = function() {
@@ -38,6 +42,7 @@ function Particle(x, y, hu, firework) {
 
   this.show = function() {
     colorMode(HSB);
+    
     if (!this.firework) {
       strokeWeight(2);
       stroke(hu, 255, 255, this.lifespan);
@@ -45,7 +50,7 @@ function Particle(x, y, hu, firework) {
       strokeWeight(4);
       stroke(hu, 255, 255);
     }
+    
     point(this.pos.x, this.pos.y);
   }
-
 }
