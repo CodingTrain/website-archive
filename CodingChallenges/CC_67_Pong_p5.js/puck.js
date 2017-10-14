d884854b172522352624a053f4d7aef84f6fc8fb
madacoo <mdcooper1705@gmail.com>
2017-10-14 10:34:57 +0800

    port of CC_67_Pong to p5.js

---
+++ CodingChallenges/CC_67_Pong_p5.js/puck.js
@@ -0,0 +1,82 @@
+class Puck {
+    constructor() {
+        this.x = width/2;
+        this.y = height/2;
+        this.xspeed = 0;
+        this.yspeed = 0;
+        this.r = 12;
+        
+        this.reset();
+    }
+    
+    checkPaddleLeft(p) {
+        if (this.y - this.r < p.y + p.h/2 &&
+            this.y + this.r > p.y - p.h/2 &&
+            this.x - this.r < p.x + p.w/2) {
+                
+            if (this.x > p.x) {
+                let diff = this.y - (p.y - p.h/2);
+                let rad = radians(45);
+                let angle = map(diff, 0, p.h, -rad, rad);
+                this.xspeed = 5 * cos(angle);
+                this.yspeed = 5 * sin(angle);
+                this.x = p.x + p.w/2 + this.r;
+            }
+            
+        }
+    }
+    checkPaddleRight(p) {
+        if (this.y - this.r < p.y + p.h/2 &&
+            this.y + this.r > p.y - p.h/2 &&
+            this.x + this.r > p.x - p.w/2) {
+                
+            if (this.x < p.x) {
+                let diff = this.y - (p.y - p.h/2);
+                let angle = map(diff, 0, p.h, radians(225), radians(135));
+                this.xspeed = 5 * cos(angle);
+                this.yspeed = 5 * sin(angle);
+                this.x = p.x - p.w/2 - this.r;
+            }
+        }
+    }
+    
+    update() {
+        this.x += this.xspeed;
+        this.y += this.yspeed;
+    }
+    
+    reset() {
+        this.x = width/2;
+        this.y = height/2;
+        let angle = random(-PI/4, PI/4);
+        this.xspeed = 5 * Math.cos(angle);
+        this.yspeed = 5 * Math.sin(angle);
+        
+        if (random(1) < 0.5) {
+            this.xspeed *= -1;
+        }
+    }
+    
+    edges() {
+        if (this.y < 0 || this.y > height) {
+            this.yspeed *= -1;
+        }
+        
+        if (this.x - this.r > width) {
+            ding.play();
+            leftscore++;
+            this.reset();
+        }
+        
+        if (this.x + this.r < 0) {
+            ding.play();
+            rightscore++;
+            this.reset();
+        }
+    }
+    
+    show() {
+        fill(255);
+        ellipse(this.x, this.y, this.r*2);
+    }
+}
