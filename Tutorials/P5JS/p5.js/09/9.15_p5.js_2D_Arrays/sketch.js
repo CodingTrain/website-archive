e1b8072dd3a73ca0c09fe9056b6cca91d99c05af
Merijn den Houting <32244117+Merijn-DH@users.noreply.github.com>
2017-10-26 12:12:02 +0200

    Update sketch.js

--- Tutorials/P5JS/p5.js/09/9.15_p5.js_2D_Arrays/sketch.js
+++ Tutorials/P5JS/p5.js/09/9.15_p5.js_2D_Arrays/sketch.js
@@ -1,36 +1,36 @@
-let cols = 10;
-let rows = 10;
+var cols = 10;
+var rows = 10;
 
 var colors = [];
 
 function make2Darray(cols, rows) {
-	var arr = new Array(cols);
-	for (let i = 0; i < arr.length; i++) {
-		arr[i] = new Array(rows);
-	}
-	return arr;
+  var arr = new Array(cols);
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
 }
 
 function setup() {
-	createCanvas(300, 300);
-	colors = make2Darray(cols, rows);
-	for (let i = 0; i < cols; i++) {
-		for (let j = 0; j < rows; j++) {
-			colors[i][j] = random(255);
-		}
-	}
+  createCanvas(300, 300);
+  colors = make2Darray(cols, rows);
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      colors[i][j] = random(255);
+    }
+  }
 }
 
 function draw() {
-	background(51);
+  background(51);
 
-	for (let i = 0; i < cols; i++) {
-		for (let j = 0; j < rows; j++) {
-			let x = i * 30;
-			let y = j * 30;
-			fill(colors[i][j]);
-			stroke(0);
-			rect(x, y, 30, 30);
-		}
-	}
+  for (var i = 0; i < cols; i++) {
+    for (var j = 0; j < rows; j++) {
+      var x = i * 30;
+      var y = j * 30;
+      fill(colors[i][j]);
+      stroke(0);
+      rect(x, y, 30, 30);
+    }
+  }
 }
