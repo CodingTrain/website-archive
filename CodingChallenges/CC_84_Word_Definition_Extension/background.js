5626b7335c7d727e90e94ad7aa12a6376f55378e
Daniel Shiffman <daniel.shiffman@gmail.com>
2017-11-20 21:03:13 +0000

    Add code from stream 107

---
+++ CodingChallenges/CC_84_Word_Definition_Extension/background.js
@@ -0,0 +1,14 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+
+console.log('background running');
+
+chrome.runtime.onMessage.addListener(receiver);
+
+window.word = "coding train";
+
+function receiver(request, sender, sendResponse) {
+  console.log(request);
+  word = request.text;
+}
