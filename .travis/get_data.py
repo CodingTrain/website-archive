93e69846e9d7caff524cd1662cd8b43d268c2b2c
Haider Ali Punjabi <haiderali@cyberservices.com>
2017-12-10 20:32:28 +0530

    Fixed rewrite error

--- .travis/get_data.py
+++ .travis/get_data.py
@@ -18,7 +18,7 @@ def make_readme(videos):
     for video in videos:
         stream_link = '* [' + video['title'] + '](https://youtu.be/' + video['video_id'] + '?list=' + os.environ.get('playlistID') + ')\n'
         lines.append(stream_link)
-    print(' '.join(lines) ,file=open('README.md','w'))
+    print(''.join(lines) ,file=open('README.md','w'))
 def get_videos(pageToken=''):
     YOUTUBE_API_URL = 'https://www.googleapis.com/youtube/v3/playlistItems?maxResults=50&pageToken=%s&part=snippet&playlistId=%s&key=' + os.environ.get('YOUTUBE_API_KEY')
     response = req.get(YOUTUBE_API_URL % (pageToken,os.environ.get('playlistID')))
