e590993313daa3bdb6ebaca8619a5c964926025e
Merijn den Houting <32244117+Merijn-DH@users.noreply.github.com>
2017-12-18 16:29:54 +0100

    Added code for 18.1-18.4 (webGL tutorials)
    Code taken from livestreams 110 and 111.

---
+++ Tutorials/P5JS/p5.js/18/18.04_p5.js_Texture/sketch.js
@@ -0,0 +1,44 @@
+let angle = 0;
+let kitten;
+let cam;
+
+function preload() {
+    kitten = loadImage('kittens/kitten0.jpg')
+}
+
+function setup() {
+    createCanvas(400, 300, WEBGL);
+    cam = createCapture(VIDEO);
+    cam.size(320, 240);
+    cam.hide();
+}
+
+function draw() {
+    let dx = mouseX - width / 2;
+    let dy = mouseY - height / 2;
+    let v = createVector(dx, dy, 0);
+    v.div(100);
+
+    // ambientLight(255);
+    directionalLight(255, 255, 255, v);
+    background(175);
+
+    push();
+    rotateX(angle);
+    rotateY(angle * 0.3);
+    rotateZ(angle * 1.2);
+
+    noStroke();
+    //ambientMaterial(0, 0, 255);
+    texture(cam);
+    //torus(100, 25);
+    box(100);
+    pop();
+
+    translate(0, 100);
+    rotateX(HALF_PI);
+    ambientMaterial(255);
+    plane(500, 500);
+
+    angle += 0.03;
+}
