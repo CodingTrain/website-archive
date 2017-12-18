aa7598b6806256758248373e2d7770afe98fac64
Merijn den Houting <32244117+Merijn-DH@users.noreply.github.com>
2017-12-19 00:23:28 +0100

    changed spacing 18.2

--- Tutorials/P5JS/p5.js/18/18.02_p5.js_Geometry/sketch.js
+++ Tutorials/P5JS/p5.js/18/18.02_p5.js_Geometry/sketch.js
@@ -1,21 +1,21 @@
 let angle = 0;
 
 function setup() {
-    createCanvas(400, 300, WEBGL);
+  createCanvas(400, 300, WEBGL);
 }
 
 function draw() {
-    background(175);
+  background(175);
 
-    rectMode(CENTER);
-    noStroke()
-    fill(0, 0, 255);
-    //translate(0, 0, mouseX);
-    rotateX(angle);
-    rotateY(angle * 0.3);
-    rotateZ(angle * 1.2);
-    //rect(0, 0, 150, 150);
-    torus(50, 10);
+  rectMode(CENTER);
+  noStroke()
+  fill(0, 0, 255);
+  //translate(0, 0, mouseX);
+  rotateX(angle);
+  rotateY(angle * 0.3);
+  rotateZ(angle * 1.2);
+  //rect(0, 0, 150, 150);
+  torus(50, 10);
 
-    angle += 0.07;
+  angle += 0.07;
 }
