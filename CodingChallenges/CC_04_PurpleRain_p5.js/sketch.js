2a7bdb0d9f1e2467c7bb0220bfaa216e5059c41b
mehedi432 <aamehedi93@gmail.com>
2018-01-25 17:24:58 +0600

    Adding window Width && Height.

--- CodingChallenges/CC_04_PurpleRain_p5.js/sketch.js
+++ CodingChallenges/CC_04_PurpleRain_p5.js/sketch.js
@@ -5,21 +5,21 @@
 
 // Purple Rain
 // (138, 43, 226)
-// (230, 230, 250) // background
+// (230, 230, 250)  background
 
 var drops = [];
 
 function setup() {
-  createCanvas(640, 360);
-  for (var i = 0; i < 500; i++) {
-    drops[i] = new Drop();
+  createCanvas( windowWidth, windowHeight );
+  for ( var i = 0; i < 500; i++ ) {
+    drops[ i ] = new Drop();
   }
 }
 
 function draw() {
-  background(230, 230, 250);
-  for (var i = 0; i < drops.length; i++) {
-    drops[i].fall();
-    drops[i].show();
+  background( 230, 230, 250 );
+  for ( var i = 0; i < drops.length; i++ ) {
+    drops[ i ].fall();
+    drops[ i ].show();
   }
 }
