03190e73b4cbdfc9cb3be354fd030f95190fafff
MeaningOf42 <biltimi@gmail.com>
2018-03-03 17:22:06 +0100

    added check for undefined properties of yaml.

--- unit_testing/coding-challange-yaml.test.js
+++ unit_testing/coding-challange-yaml.test.js
@@ -1,6 +1,23 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
-let verbose = true;
+let verbose = false;
+
+function checkHasUndefinedReccursive(value) {
+  // check if object has undefined or null properties
+  // if it doesn't and it has properties which are objects,
+  // recursivly check if they are undefined
+  if (value == null) { //this checks for undefined as undefined == null is true
+    return true
+  }
+  if (value instanceof Object) {
+    accumilator = false;
+    for (let key in value) {
+      accumilator = accumilator || checkHasUndefinedReccursive(value[key]);
+    }
+    return accumilator;
+  }
+  return false;
+}
 
 let CodingChallangesDir = "../_CodingChallenges";
 
@@ -22,7 +39,7 @@ files.forEach((file)=>{
     }
   });
 
-  test("Check that contents of _CodingChallenges/"+file+" has oppening and closing '---'s", () => {
+  test("Check that contents of "+CodingChallangesDir+"/"+file+" has oppening and closing '---'s", () => {
     let contents = fs.readFileSync(CodingChallangesDir+"/"+file, 'utf8');
     expect(contents.split("---").length-1).toBe(2);
     if (verbose) {
@@ -30,7 +47,7 @@ files.forEach((file)=>{
     }
   });
 
-  test("Check that contents of _CodingChallenges/"+file+" has YAML section at beggining", () => {
+  test("Check that contents of "+CodingChallangesDir+"/"+file+" has YAML section at beggining", () => {
     let contents = fs.readFileSync(CodingChallangesDir+"/"+file, 'utf8');
     expect(contents.split("---")[0].length).toBe(0);
     if (verbose) {
@@ -38,7 +55,7 @@ files.forEach((file)=>{
     }
   });
 
-  test("Check that contents of _CodingChallenges/"+file+" does not have empty YAML section", () => {
+  test("Check that contents of "+CodingChallangesDir+"/"+file+" does not have empty YAML section", () => {
     let contents = fs.readFileSync(CodingChallangesDir+"/"+file, 'utf8');
     expect(contents.split("---")[1].length>0).toBe(true);
     if (verbose) {
@@ -46,7 +63,7 @@ files.forEach((file)=>{
     }
   });
 
-  test("Check that YAML contents of _CodingChallenges/"+file+" valid", () => {
+  test("Check that YAML contents of "+CodingChallangesDir+"/"+file+" valid", () => {
     let contents = fs.readFileSync(CodingChallangesDir+"/"+file, 'utf8');
     let yamlContents = contents.split("---")[1];
     const frontYaml = yaml.safeLoad(yamlContents);
@@ -58,12 +75,26 @@ files.forEach((file)=>{
     }
   });
 
-  test("Check that YAML contents of _CodingChallenges/"+file
+  test("Check that YAML contents of "+CodingChallangesDir+"/"+file
   +" have required properties", () => {
     let contents = fs.readFileSync(CodingChallangesDir+"/"+file, 'utf8');
     let yamlContents = contents.split("---")[1];
     const frontYaml = yaml.safeLoad(yamlContents);
+    let requiredProperties = ["title", "video_number", "date", "video_id"];
+    if (file=="index.md") {
+      requiredProperties = ["title", "layout"];
+    }
+    for (let key of requiredProperties) {
+      expect(frontYaml.hasOwnProperty(key)).toBe(true);
+    }
+  });
 
-  })
+  test("Check no undefined properties of object from YAML of "+
+    CodingChallangesDir+"/"+file, () => {
+    let contents = fs.readFileSync(CodingChallangesDir+"/"+file, 'utf8');
+    let yamlContents = contents.split("---")[1];
+    const frontYaml = yaml.safeLoad(yamlContents);
+    expect(checkHasUndefinedReccursive(frontYaml)).toBe(false);
+  });
 
 });
