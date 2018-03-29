cb94e138366fdfc983e596426683bf293e5ec71f
WakeMeAtThree <wakemeatthree@gmail.com>
2018-03-29 20:23:35 +0400

    Fixing the issue where snake can move backward on itself

--- CodingChallenges/CC_03_Snake_game_p5.js/sketch.js
+++ CodingChallenges/CC_03_Snake_game_p5.js/sketch.js
@@ -47,13 +47,13 @@ function draw() {
 
 
 function keyPressed() {
-  if (keyCode === UP_ARROW) {
+  if (keyCode === UP_ARROW && s.yspeed != 1) {
     s.dir(0, -1);
-  } else if (keyCode === DOWN_ARROW) {
+  } else if (keyCode === DOWN_ARROW && s.yspeed != -1) {
     s.dir(0, 1);
-  } else if (keyCode === RIGHT_ARROW) {
+  } else if (keyCode === RIGHT_ARROW && s.xspeed != -1) {
     s.dir(1, 0);
-  } else if (keyCode === LEFT_ARROW) {
+  } else if (keyCode === LEFT_ARROW && s.xspeed != 1) {
     s.dir(-1, 0);
   }
 }
