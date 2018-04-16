be097b5c17e27c4fb1f2b8deda7e3e78c5d6afb3
Daniel Shiffman <daniel.shiffman@gmail.com>
2018-04-16 09:14:21 -0400

    matching coding challenge 100 base code

--- CodingChallenges/CC_31_FlappyBird_p5.js/bird.js
+++ CodingChallenges/CC_31_FlappyBird_p5.js/bird.js
@@ -7,8 +7,8 @@ function Bird() {
   this.y = height/2;
   this.x = 64;
 
-  this.gravity = 0.6;
-  this.lift = -15;
+  this.gravity = 0.7;
+  this.lift = -12;
   this.velocity = 0;
 
   this.show = function() {
