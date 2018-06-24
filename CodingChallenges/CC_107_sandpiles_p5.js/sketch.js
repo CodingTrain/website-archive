8e614037814391ceb605aae0963724c761bd2550
Frode Austvik <frode@documaster.no>
2018-06-24 16:45:33 +0200

    Improve the framerate of the p5.js port of coding challenge 107.
    This improves the initial framerate from ~0.4 fps to ~8 fps (on my PC),
    while staying fairly close to the original code.
    
    These are the main optimizations/speedups that were applied:
    * Not using color() and set() to update the pixels.
    * Not recreating the data arrays for each topple() iteration.
    * Faster copying of the previous data (fewer comparisons).
    * Fewer topple() iterations per frame.
    * Smaller canvas.

--- CodingChallenges/CC_107_sandpiles_p5.js/sketch.js
+++ CodingChallenges/CC_107_sandpiles_p5.js/sketch.js
@@ -5,24 +5,33 @@
 // Sandpiles
 // https://youtu.be/diGjw5tghYU
 
+let defaultColor = [255, 0, 0];
+let colors = [
+  [255, 255,   0],
+  [  0, 185,  63],
+  [  0, 104, 255],
+  [122,   0, 229]
+];
+
 let sandpiles;
+let nextpiles;
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(600, 600);
   pixelDensity(1);
+
   sandpiles = new Array(width).fill().map(i => new Array(height).fill(0));
+  nextpiles = new Array(width).fill().map(i => new Array(height).fill(0));
+
   sandpiles[width/2][height/2] = 1000000000;
+
+  background(defaultColor[0], defaultColor[1], defaultColor[2]);
 }
 
 function topple() {
-  let nextpiles = [];
   for (let x = 0; x < width; x++) {
-    nextpiles[x] = new Array(height).fill(0);
     for (let y = 0; y < height; y++) {
-      let num = sandpiles[x][y];
-      if (num < 4) {
-        nextpiles[x][y] = sandpiles[x][y];
-      }
+      nextpiles[x][y] = sandpiles[x][y];
     }
   }
 
@@ -30,7 +39,7 @@ function topple() {
     for (let y = 0; y < height; y++) {
       let num = sandpiles[x][y];
       if (num >= 4) {
-        nextpiles[x][y] += (num - 4);
+        nextpiles[x][y] -= 4;
         if (x+1 < width)
           nextpiles[x+1][y]++;
         if (x-1 >= 0)
@@ -43,7 +52,9 @@ function topple() {
     }
   }
 
+  let tmp = sandpiles;
   sandpiles = nextpiles;
+  nextpiles = tmp;
 }
 
 function render() {
@@ -51,18 +62,22 @@ function render() {
   for (let x = 0; x < width; x++) {
     for (let y = 0; y < height; y++) {
       let num = sandpiles[x][y];
-      let col = color(255, 0, 0);
+      let col = defaultColor;
       if (num == 0) {
-        col = color(255, 255, 0);
+        col = colors[0];
       } else if (num == 1) {
-        col = color(0, 185, 63);
+        col = colors[1];
       } else if (num == 2) {
-        col = color(0, 104, 255);
+        col = colors[2];
       } else if (num == 3) {
-        col = color(122, 0, 229);
+        col = colors[3];
       }
 
-      set(x, y, col);
+      let pix = (x + y * width) * 4;
+      pixels[pix    ] = col[0];
+      pixels[pix + 1] = col[1];
+      pixels[pix + 2] = col[2];
+      // pixels[pix + 3] = 255;
     }
   }
 
@@ -71,7 +86,8 @@ function render() {
 
 function draw() {
   render();
-  for (let i = 0; i < 100; i++) {
+
+  for (let i = 0; i < 50; i++) {
     topple();
   }
 }
