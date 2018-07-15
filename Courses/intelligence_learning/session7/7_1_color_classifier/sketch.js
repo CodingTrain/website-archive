1a126163a27b9c2f33b6dfa9b0c92c3a2aa8091e
Luke Nonas-Hunter <39748544+nonas-hunter@users.noreply.github.com>
2018-07-15 18:44:38 -0400

    Code for Video 1 of Session 7 Intelligence and Learning
    This folder contains the code at the end of Video 1 of Session 7

---
+++ Courses/intelligence_learning/session7/7_1_color_classifier/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(100, 100);
+  let r = floor(random(256));
+  let g = floor(random(256));
+  let b = floor(random(256));
+  background(r, g, b);
+
+  let dropdown = createSelect();
+  dropdown.option('red-ish');
+  dropdown.option('blue-ish');
+  dropdown.option('green-ish');
+
+  let submit = createButton('submit');
+  submit.mousePressed(sendData);
+
+  function sendData() {
+    // send this data to something?
+  }
+}
