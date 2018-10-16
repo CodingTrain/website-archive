231df3400e7cbefd9ab1fecfb14cea51b6ef7e7d
Zhanet Ivanova <ivanova.zhanet@gmail.com>
2018-10-16 22:16:38 +0100

    Added header information to sketch.js

--- CodingChallenges/CC_117_SevenSegmentDisplay_p5.js/sketch.js
+++ CodingChallenges/CC_117_SevenSegmentDisplay_p5.js/sketch.js
@@ -1,3 +1,13 @@
+// Daniel Shiffman
+// http://youtube.com/thecodingtrain
+// http://codingtra.in
+
+// Coding Challenge 117: Seven Segment Display
+// https://youtu.be/MlRlgbrAVOs
+
+// p5.js editor version: 0.7.2 (September 02, 2018)
+// https://editor.p5js.org/codingtrain/sketches/HkP4CF09X
+
 let nums = [0x7e, 0x30, 0x6d, 0x79, 0x33, 0x5b, 0x5f, 0x70, 0x7f, 0x7b];
 let index = 0;
 
