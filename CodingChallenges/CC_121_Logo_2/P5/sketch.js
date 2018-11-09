3711d8d5cd45272eb37939ca6be84a0e6a5337d7
Daniel Shiffman <daniel@shiffman.net>
2018-11-09 16:09:44 -0500

    spaces not tabs

--- CodingChallenges/CC_121_Logo_2/P5/sketch.js
+++ CodingChallenges/CC_121_Logo_2/P5/sketch.js
@@ -5,38 +5,37 @@ let editor;
 let turtle;
 
 function setup() {
-	createCanvas(200, 200);
-	angleMode(DEGREES);
-	background(0);
-	turtle = new Turtle(100, 100, 0);
-	editor = select('#code');
-	editor.input(goTurtle);
-	goTurtle();
+  createCanvas(200, 200);
+  angleMode(DEGREES);
+  background(0);
+  turtle = new Turtle(100, 100, 0);
+  editor = select("#code");
+  editor.input(goTurtle);
+  goTurtle();
 }
 
 function execute(commands) {
-	for (let command of commands) {
-		let name = command.name;
-		let arg = command.arg;
-		if (name === 'repeat') {
-			for (let i = 0; i < arg; i++) {
-				execute(command.commands);
-			}
-		} else {
-			commandLookUp[name](arg);
-		}
-	}
+  for (let command of commands) {
+    let name = command.name;
+    let arg = command.arg;
+    if (name === "repeat") {
+      for (let i = 0; i < arg; i++) {
+        execute(command.commands);
+      }
+    } else {
+      commandLookUp[name](arg);
+    }
+  }
 }
 
-
 function goTurtle() {
-	background(0);
-	push();
-	turtle.reset();
-	let code = editor.value();
-	let parser = new Parser(code);
-	let commands = parser.parse();
-	console.log(commands);
-	execute(commands);
-	pop();
+  background(0);
+  push();
+  turtle.reset();
+  let code = editor.value();
+  let parser = new Parser(code);
+  let commands = parser.parse();
+  console.log(commands);
+  execute(commands);
+  pop();
 }
