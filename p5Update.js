f71d364887589bc6ae208d16b3dc696b95209f2b
Nico Finkernagel <nico@gruselhaus.com>
2019-03-15 23:18:33 +0100

    added ignored files feature

--- p5Update.js
+++ p5Update.js
@@ -7,53 +7,62 @@ const replace = require("replace-in-file");
 //Specify the release you want to update to
 const release = "0.7.3";
 
+const paths = ["./**/index.html"];
+const ignored_files = ["./_site/**/*.html"];
+
 const p5_js = {
   name: "p5.js",
-  files: ["./CodingChallenges/**/index.html"],
+  files: paths,
   from: new RegExp(
     "https://cdnjs.cloudflare.com/ajax/libs/p5.js/[0-9].[0-9].[0-9]/p5.js"
   ),
-  to: `https://cdnjs.cloudflare.com/ajax/libs/p5.js/${release}/p5.min.js`
+  to: `https://cdnjs.cloudflare.com/ajax/libs/p5.js/${release}/p5.min.js`,
+  ignore: ignored_files
 };
 const p5_sound_js = {
   name: "p5.sound.js",
-  files: ["./CodingChallenges/**/index.html"],
+  files: paths,
   from: new RegExp(
     "https://cdnjs.cloudflare.com/ajax/libs/p5.js/[0-9].[0-9].[0-9]/addons/p5.sound.js"
   ),
-  to: `https://cdnjs.cloudflare.com/ajax/libs/p5.js/${release}/addons/p5.sound.min.js`
+  to: `https://cdnjs.cloudflare.com/ajax/libs/p5.js/${release}/addons/p5.sound.min.js`,
+  ignore: ignored_files
 };
 const p5_dom_js = {
   name: "p5.dom.js",
-  files: ["./CodingChallenges/**/index.html"],
+  files: paths,
   from: new RegExp(
     "https://cdnjs.cloudflare.com/ajax/libs/p5.js/[0-9].[0-9].[0-9]/addons/p5.dom.js"
   ),
-  to: `https://cdnjs.cloudflare.com/ajax/libs/p5.js/${release}/addons/p5.dom.min.js`
+  to: `https://cdnjs.cloudflare.com/ajax/libs/p5.js/${release}/addons/p5.dom.min.js`,
+  ignore: ignored_files
 };
 const p5_min_js = {
   name: "p5.min.js",
-  files: ["./CodingChallenges/**/index.html"],
+  files: paths,
   from: new RegExp(
     "https://cdnjs.cloudflare.com/ajax/libs/p5.js/[0-9].[0-9].[0-9]/p5.min.js"
   ),
-  to: `https://cdnjs.cloudflare.com/ajax/libs/p5.js/${release}/p5.min.js`
+  to: `https://cdnjs.cloudflare.com/ajax/libs/p5.js/${release}/p5.min.js`,
+  ignore: ignored_files
 };
 const p5_sound_min_js = {
   name: "p5.sound.min.js",
-  files: ["./CodingChallenges/**/index.html"],
+  files: paths,
   from: new RegExp(
     "https://cdnjs.cloudflare.com/ajax/libs/p5.js/[0-9].[0-9].[0-9]/addons/p5.sound.min.js"
   ),
-  to: `https://cdnjs.cloudflare.com/ajax/libs/p5.js/${release}/addons/p5.sound.min.js`
+  to: `https://cdnjs.cloudflare.com/ajax/libs/p5.js/${release}/addons/p5.sound.min.js`,
+  ignore: ignored_files
 };
 const p5_dom_min_js = {
   name: "p5.dom.min.js",
-  files: ["./CodingChallenges/**/index.html"],
+  files: paths,
   from: new RegExp(
     "https://cdnjs.cloudflare.com/ajax/libs/p5.js/[0-9].[0-9].[0-9]/addons/p5.dom.min.js"
   ),
-  to: `https://cdnjs.cloudflare.com/ajax/libs/p5.js/${release}/addons/p5.dom.min.js`
+  to: `https://cdnjs.cloudflare.com/ajax/libs/p5.js/${release}/addons/p5.dom.min.js`,
+  ignore: ignored_files
 };
 const options = [
   p5_js,
