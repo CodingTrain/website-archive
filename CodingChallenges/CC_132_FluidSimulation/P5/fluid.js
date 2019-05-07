92b01e95aeace39e8e9d080ef74612a373fd1d17
Jactus <dw1920@nyu.edu>
2019-05-07 00:40:48 -0400

    Add P5 version of CC132

---
+++ CodingChallenges/CC_132_FluidSimulation/P5/fluid.js
@@ -0,0 +1,107 @@
+/// <reference path="./p5.global-mode.d.ts" />
+
+let N = 256;
+let iter = 16;
+let SCALE = 4;
+let t = 0;
+
+// function to use 1D array and fake the extra two dimensions --> 3D
+function IX(x, y) {
+    return x + y*N;
+}
+
+// Fluid cube class
+class Fluid {
+    constructor(dt, diffusion, viscosity) {
+        this.size = N;
+        this.dt = dt;
+        this.diff = diffusion;
+        this.visc = viscosity;
+
+        this.s = new Array(N*N).fill(0);
+        this.density = new Array(N*N).fill(0);
+
+        this.Vx = new Array(N*N).fill(0);
+        this.Vy = new Array(N*N).fill(0);
+
+        this.Vx0 = new Array(N*N).fill(0);
+        this.Vy0 = new Array(N*N).fill(0);
+
+        console.log(this.Vy)
+
+
+        // this.canvas = canvas;
+    }
+
+    // step method
+    step() {
+        let N = this.size;
+        let visc = this.visc;
+        let diff = this.diff;
+        let dt = this.dt;
+        let Vx = this.Vx;
+        let Vy = this.Vy;
+        let Vx0 = this.Vx0;
+        let Vy0  = this.Vy0;
+        let s = this.s;
+        let density = this.density;
+
+
+        diffuse(1, Vx0, Vx, visc, dt);
+        diffuse(2, Vy0, Vy, visc, dt);
+
+        project(Vx0, Vy0, Vx, Vy);
+
+        advect(1, Vx, Vx0, Vx0, Vy0, dt);
+        advect(2, Vy, Vy0, Vx0, Vy0, dt);
+
+        project(Vx, Vy, Vx0, Vy0);
+        diffuse(0, s, density, diff, dt);
+        advect(0, density, s, Vx, Vy, dt);
+    }
+
+    // method to add density
+    addDensity(x, y, amount) {
+        let index = IX(x, y);
+        this.density[index] += amount;
+    }
+
+    // method to add velocity
+    addVelocity(x, y, amountX, amountY) {
+        let index = IX(x, y);
+        this.Vx[index] += amountX;
+        this.Vy[index] += amountY;
+    }
+
+    // function to render density
+    renderD() {
+        for (let i = 0; i < N; i++) {
+            for (let j = 0; j < N; j++) {
+                let x = i * SCALE;
+                let y = j * SCALE;
+                let d = this.density[IX(i, j)];
+                fill(d);
+                noStroke();
+                rect(x, y, SCALE, SCALE);
+            }
+        }
+    }
+    
+    // function to render velocity
+    renderV() {
+        for (let i = 0; i < N; i++) {
+            for (let j = 0; j < N; j++) {
+                let x = i * SCALE;
+                let y = j * SCALE;
+                let vx = this.Vx[IX(i, j, k)];
+                let vy = this.Vy[IX(i, j, k)];
+                this.canvas.stroke(0);
+        
+                if (!(abs(vx) < 0.1 && abs(vy) <= 0.1)) {
+                    line(x, y, x+vx*SCALE, y+vy*SCALE );
+                }
+            }
+        }
+    }
+}
+
