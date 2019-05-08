8a585f03f87386e9854dcc2cccc5fa7b8f53fa56
Daniel Shiffman <daniel@shiffman.net>
2019-05-08 10:50:44 -0400

    Coding Challenge 145 Ray Casting (#1319)
    * cc 145 ray casting code
    
    * markdown file and code fixes
    
    * description and extra text

---
+++ CodingChallenges/CC_145_Ray_Casting/P5/ray.js
@@ -0,0 +1,55 @@
+// Daniel Shiffman
+// https://thecodingtrain.com/CodingChallenges/145-2d-ray-casting.html
+// https://youtu.be/TOEi6T2mtHo
+
+// 2D Ray Casting
+// https://editor.p5js.org/codingtrain/sketches/Nqsq3DFv-
+
+class Ray {
+  constructor(pos, angle) {
+    this.pos = pos;
+    this.dir = p5.Vector.fromAngle(angle);
+  }
+
+  lookAt(x, y) {
+    this.dir.x = x - this.pos.x;
+    this.dir.y = y - this.pos.y;
+    this.dir.normalize();
+  }
+
+  show() {
+    stroke(255);
+    push();
+    translate(this.pos.x, this.pos.y);
+    line(0, 0, this.dir.x * 10, this.dir.y * 10);
+    pop();
+  }
+
+  cast(wall) {
+    const x1 = wall.a.x;
+    const y1 = wall.a.y;
+    const x2 = wall.b.x;
+    const y2 = wall.b.y;
+
+    const x3 = this.pos.x;
+    const y3 = this.pos.y;
+    const x4 = this.pos.x + this.dir.x;
+    const y4 = this.pos.y + this.dir.y;
+
+    const den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
+    if (den == 0) {
+      return;
+    }
+
+    const t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den;
+    const u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den;
+    if (t > 0 && t < 1 && u > 0) {
+      const pt = createVector();
+      pt.x = x1 + t * (x2 - x1);
+      pt.y = y1 + t * (y2 - y1);
+      return pt;
+    } else {
+      return;
+    }
+  }
+}
---
+++ _CodingChallenges/145-2d-ray-casting.md
@@ -0,0 +1,28 @@
+---
+title: "Ray Casting 2D"
+video_number: 145
+date: 2019-05-08
+video_id: TOEi6T2mtHo
+web_editor: Nqsq3DFv-
+repository: CC_145_Ray_Casting
+
+links:
+  - title: "SIGHT & LIGHT by Nicky Case"
+    url: https://ncase.me/sight-and-light/
+  - title: "2d Visibility from Red Blob Games"
+    url: "https://www.redblobgames.com/articles/visibility/"
+  - title: "p5.collide2D"
+    url: "https://github.com/bmoren/p5.collide2D"
+  - title: "Line-line Intersection"
+    url: "https://en.wikipedia.org/wiki/Line%E2%80%93line_intersection"
+
+videos:
+  - title: "Coding Adventure: Ray Marching"
+    author: "Sebastian Lague"
+    video_id: "Cp5WWtMoeKg"
+  - title: "Matrix Math"
+    author: "Coding Train"
+    video_id: "uSzGdfdOoG8"
+---
+
+In this video, I implement a basic ray casting engine with line segment "surfaces" and vector "rays." The result simulates a light source casting shadows in a 2D canvas.
