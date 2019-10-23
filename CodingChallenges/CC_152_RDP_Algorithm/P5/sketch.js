582d4831b757a8cd230d0f6ea920a85df31932d7
Daniel Shiffman <daniel@shiffman.net>
2019-10-23 13:48:11 -0400

    Coding Challenge 152 (#1791)
    * adding challenge 152 RDP
    
    * removing css file
    
    * fix video link to local path
    
    * fixed link rnn snowflake challenge
    
    * adding scalar projection example

---
+++ CodingChallenges/CC_152_RDP_Algorithm/P5/sketch.js
@@ -0,0 +1,110 @@
+// Daniel Shiffman
+// https://thecodingtrain.com/CodingChallenges/152-rdp-algorithm.html
+// https://youtu.be/nSYw9GrakjY
+// https://editor.p5js.org/codingtrain/sketches/SQjSugKn6
+
+const allPoints = [];
+let epsilon = 0;
+
+function setup() {
+  createCanvas(600, 400);
+  for (let x = 0; x < width; x++) {
+    const xval = map(x, 0, width, 0, 5);
+    const yval = exp(-xval) * cos(TWO_PI * xval);
+    const y = map(yval, -1, 1, height, 0);
+    allPoints.push(createVector(x, y));
+  }
+}
+
+function rdp(startIndex, endIndex, allPoints, rdpPoints) {
+  const nextIndex = findFurthest(allPoints, startIndex, endIndex);
+  if (nextIndex > 0) {
+    if (startIndex != nextIndex) {
+      rdp(startIndex, nextIndex, allPoints, rdpPoints);
+    }
+    rdpPoints.push(allPoints[nextIndex]);
+    if (endIndex != nextIndex) {
+      rdp(nextIndex, endIndex, allPoints, rdpPoints);
+    }
+  }
+}
+
+
+function findFurthest(points, a, b) {
+  let recordDistance = -1;
+  const start = points[a];
+  const end = points[b];
+  let furthestIndex = -1;
+  for (let i = a + 1; i < b; i++) {
+    const currentPoint = points[i];
+    const d = lineDist(currentPoint, start, end);
+    if (d > recordDistance) {
+      recordDistance = d;
+      furthestIndex = i;
+    }
+  }
+  if (recordDistance > epsilon) {
+    return furthestIndex;
+  } else {
+    return -1;
+  }
+}
+
+
+function lineDist(c, a, b) {
+  const norm = scalarProjection(c, a, b);
+  return p5.Vector.dist(c, norm);
+}
+
+function scalarProjection(p, a, b) {
+  const ap = p5.Vector.sub(p, a);
+  const ab = p5.Vector.sub(b, a);
+  ab.normalize(); // Normalize the line
+  ab.mult(ap.dot(ab));
+  const normalPoint = p5.Vector.add(a, ab);
+  return normalPoint;
+}
+
+
+function draw() {
+  background(0);
+
+  const rdpPoints = [];
+
+  const total = allPoints.length;
+  const start = allPoints[0];
+  const end = allPoints[total - 1];
+  rdpPoints.push(start);
+  rdp(0, total - 1, allPoints, rdpPoints);
+  rdpPoints.push(end);
+  //console.log(allPoints.size(), rdpPoints.length);
+
+  epsilon += 0.01;
+  if (epsilon > 100) {
+    epsilon = 0;
+  }
+
+  //translate(0, -10);
+  stroke(255, 0, 255);
+  strokeWeight(4);
+  noFill();
+  beginShape();
+  for (let v of allPoints) {
+    vertex(v.x, v.y);
+  }
+  endShape();
+
+  //translate(0, 20);
+  stroke(255);
+  strokeWeight(2);
+  beginShape();
+  for (let v of rdpPoints) {
+    vertex(v.x, v.y);
+  }
+  endShape();
+  fill(255);
+  noStroke();
+  textSize(24);
+  text("epsilon: " + nf(epsilon, 2, 2), 20, 25);
+  text("n: " + rdpPoints.length, 20, 50);
+}
---
+++ _CodingChallenges/152-rdp-algorithm.md
@@ -0,0 +1,27 @@
+---
+title: "RDP Algorithm"
+video_number: 152
+date: 2019-10-21
+video_id: nSYw9GrakjY
+web_editor: SQjSugKn6
+repository: CC_152_RDP_Algorithm
+
+links:
+  - title: "Ramer-Douglas-Peucker Algorithm on Wikipedia"
+    url: https://en.wikipedia.org/wiki/Ramer%E2%80%93Douglas%E2%80%93Peucker_algorithm
+  - title: "Nature of Code Chapter 6"
+    url: "https://natureofcode.com/book/chapter-6-autonomous-agents/"
+  - title: "Scalar Projection Processing Example"
+    url: "https://github.com/nature-of-code/noc-examples-processing/tree/master/chp06_agents/SimpleScalarProjection"
+
+videos:
+  - title: "Challenge #128: Sketch-RNN Snowflakes"
+    author: "The Coding Train"
+    url: "/CodingChallenges/128-sketchrnn-snowflakes"
+  - title: "Live Stream #185"
+    author: "The Coding Train"
+    video_id: "5S87w8IqSHI"
+---
+
+The Ramer–Douglas–Peucker algorithm (aka "iterative end-point fit algorithm"), takes a curve composed of line segments and reduces the fidelty to a "lower fidelity" curve with fewer points.
+
