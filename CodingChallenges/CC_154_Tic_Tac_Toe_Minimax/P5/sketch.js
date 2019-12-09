bf48885c78df441086663409f953c710c1334bb5
Violet <violetcraze@protonmail.com>
2019-12-09 13:25:17 -0800

    updating headers

--- CodingChallenges/CC_154_Tic_Tac_Toe_Minimax/P5/sketch.js
+++ CodingChallenges/CC_154_Tic_Tac_Toe_Minimax/P5/sketch.js
@@ -1,4 +1,5 @@
-// Daniel Shiffman
+// Tic Tac Toe AI with Minimax Algorithm
+// The Coding Train / Daniel Shiffman
 // https://thecodingtrain.com/CodingChallenges/154-tic-tac-toe-minimax.html
 // https://youtu.be/I64-UTORVfU
 // https://editor.p5js.org/codingtrain/sketches/0zyUhZdJD
