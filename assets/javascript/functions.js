33825d62cf7c3ee067d5fd9a57e47dc7aa52c4ac
Phuc Duong <phuc.duongdinh@Gmail.com>
2020-01-07 20:38:47 -0500

    Fix for safari issue (#2037)
    * updated nav bar styling
    
    * fixed safari issue
    
    Co-authored-by: Nico Finkernagel <nico@gruselhaus.com>

--- assets/javascript/functions.js
+++ assets/javascript/functions.js
@@ -1,37 +1,38 @@
 /** The YouTube player on the current page. */
 let CURRENT_PAGE_PLAYER = null;
 
-
 /**
  * Skips the video to the timecode defined as the hash component of the URL.
  * This only happens if the timecode is bigger than zero.
  */
 function skipVideoToHashTimecode() {
   const seconds = timecodeToSeconds(location.hash.substr(1));
-  if (CURRENT_PAGE_PLAYER !== null && seconds > 0) CURRENT_PAGE_PLAYER.seekTo(seconds);
-}
-
+  if (CURRENT_PAGE_PLAYER !== null && seconds > 0) 
+    CURRENT_PAGE_PLAYER.seekTo(seconds);
+  }
 
 /**
  * Wait until the YouTube Iframe-API has loaded.
  */
 function onYouTubeIframeAPIReady() {
-  if (document.getElementById('video-player') === null) return;
-
+  if (document.getElementById('video-player') === null) 
+    return;
+  
   CURRENT_PAGE_PLAYER = new YT.Player('video-player', {
-    videoId: document.getElementById('video-player').dataset.videoid,
+    videoId: document
+      .getElementById('video-player')
+      .dataset
+      .videoid,
     events: {
       onReady: skipVideoToHashTimecode
     }
   });
 }
 
-
 /**
  * Skip the video when the hash component of the URL changes.
  */
 window.addEventListener('hashchange', skipVideoToHashTimecode);
-
 
 /**
  * Convert youtube time codes (format: HH:MM:SS or MM:SS or SS) to seconds.
@@ -44,13 +45,12 @@ function timecodeToSeconds(timecode) {
   let minutes = 1;
 
   while (timeComponents.length > 0) {
-    seconds += minutes * +timeComponents.pop();
+    seconds += minutes * + timeComponents.pop();
     minutes *= 60;
   }
 
   return seconds;
 }
-
 
 class NavApplication {
 
@@ -65,25 +65,45 @@ class NavApplication {
 
     this.subMenuButtons = document.getElementsByClassName('submenu-button');
     this.subMenus = document.getElementsByClassName('submenu');
-    
-    this.handleKeydown = this.handleKeydown.bind(this);
-    this.toggleMobileNav = this.toggleMobileNav.bind(this);
-    this.toggleSubMenu = this.toggleSubMenu.bind(this);
-    this.closeSubMenus = this.closeSubMenus.bind(this);
-    this.enableNavLinks = this.enableNavLinks.bind(this);
-    this.disableNavLinks = this.disableNavLinks.bind(this);
-    
-    this.navButton.addEventListener('click', this.toggleMobileNav);
-    this.navMenu.addEventListener('keydown', this.handleKeydown);
-    this.navButton.setAttribute('aria-label', 'Open navigation menu');
+
+    this.handleKeydown = this
+      .handleKeydown
+      .bind(this);
+    this.toggleMobileNav = this
+      .toggleMobileNav
+      .bind(this);
+    this.toggleSubMenu = this
+      .toggleSubMenu
+      .bind(this);
+    this.closeSubMenus = this
+      .closeSubMenus
+      .bind(this);
+    this.enableNavLinks = this
+      .enableNavLinks
+      .bind(this);
+    this.disableNavLinks = this
+      .disableNavLinks
+      .bind(this);
+
+    this
+      .navButton
+      .addEventListener('click', this.toggleMobileNav);
+    this
+      .navMenu
+      .addEventListener('keydown', this.handleKeydown);
+    this
+      .navButton
+      .setAttribute('aria-label', 'Open navigation menu');
     this.setUpBackToTopButton();
-    
+
     this.setupSubMenus();
   }
 
   setupSubMenus() {
     for (let i = 0; i < this.subMenuButtons.length; i++) {
-      this.subMenuButtons[i].addEventListener('click', this.toggleSubMenu);
+      this
+        .subMenuButtons[i]
+        .addEventListener('click', this.toggleSubMenu);
     }
 
     this.closeSubMenus(); // sets the open text
@@ -93,13 +113,17 @@ class NavApplication {
     const button = event.srcElement;
     const forAttribute = button.getAttribute('for');
     const subMenu = document.getElementById(forAttribute);
-    const openMenu = !subMenu.classList.contains('active');
+    const openMenu = !subMenu
+      .classList
+      .contains('active');
     this.closeSubMenus();
     if (openMenu) {
       this.escapeSubMenu = true;
 
       button.setAttribute('aria-label', `Close ${forAttribute.split('-')[0]} Submenu`);
-      subMenu.classList.add('active');
+      subMenu
+        .classList
+        .add('active');
       subMenu.removeAttribute('aria-hidden');
 
       ([...(subMenu.querySelectorAll('a'))]).forEach(el => {
@@ -110,12 +134,22 @@ class NavApplication {
 
   closeSubMenus() {
     for (let i = 0; i < this.subMenuButtons.length; i++) {
-      const name = this.subMenuButtons[i].getAttribute('for').split('-')[0];
-      this.subMenuButtons[i].setAttribute('aria-label', `Open ${name} Submenu`);
+      const name = this
+        .subMenuButtons[i]
+        .getAttribute('for')
+        .split('-')[0];
+      this
+        .subMenuButtons[i]
+        .setAttribute('aria-label', `Open ${name} Submenu`);
     }
     for (let i = 0; i < this.subMenus.length; i++) {
-      this.subMenus[i].classList.remove('active');
-      this.subMenus[i].setAttribute('aria-hidden', 'true');
+      this
+        .subMenus[i]
+        .classList
+        .remove('active');
+      this
+        .subMenus[i]
+        .setAttribute('aria-hidden', 'true');
 
       ([...(this.subMenus[i].querySelectorAll('a'))]).forEach(el => {
         el.setAttribute('tabIndex', -1);
@@ -130,7 +164,9 @@ class NavApplication {
         this.closeSubMenus();
       } else {
         this.disableNavLinks();
-        this.navButton.focus();
+        this
+          .navButton
+          .focus();
       }
     }
   }
@@ -144,52 +180,85 @@ class NavApplication {
   }
 
   enableNavLinks() {
-    this.navButton.setAttribute('aria-label', 'Close navigation menu');
-    this.navMenu.classList.add('active');
-    this.mbBackdrop.classList.add('active');
-    this.navButton.classList.add('active');
+    this
+      .navButton
+      .setAttribute('aria-label', 'Close navigation menu');
+    this
+      .navMenu
+      .classList
+      .add('active');
+    this
+      .mbBackdrop
+      .classList
+      .add('active');
+    this
+      .navButton
+      .classList
+      .add('active');
 
-    this.navMenu.removeAttribute('aria-hidden');
-    this.navLinks.forEach((el) => {
-      el.removeAttribute('tabIndex');
-    });
+    this
+      .navMenu
+      .removeAttribute('aria-hidden');
+    this
+      .navLinks
+      .forEach((el) => {
+        el.removeAttribute('tabIndex');
+      });
     setTimeout(() => {
-      this.navLinks[0].focus();
+      this
+        .navLinks[0]
+        .focus();
     }, 200);
   }
 
   disableNavLinks() {
-    this.navButton.setAttribute('aria-label', 'Open navigation menu');
-    this.navMenu.classList.remove('active');
-    this.mbBackdrop.classList.remove('active');
-    this.navButton.classList.remove('active');
+    this
+      .navButton
+      .setAttribute('aria-label', 'Open navigation menu');
+    this
+      .navMenu
+      .classList
+      .remove('active');
+    this
+      .mbBackdrop
+      .classList
+      .remove('active');
+    this
+      .navButton
+      .classList
+      .remove('active');
 
-    this.navMenu.setAttribute('aria-hidden', 'true');
-    this.navLinks.forEach((el) => {
-      el.setAttribute('tabIndex', '-1');
-    });
+    this
+      .navMenu
+      .setAttribute('aria-hidden', 'true');
+    this
+      .navLinks
+      .forEach((el) => {
+        el.setAttribute('tabIndex', '-1');
+      });
   }
 
   setUpBackToTopButton() {
     const backToTopButton = document.querySelector('.back-to-top-button');
     document.addEventListener('scroll', () => {
       if (window.scrollY > 1000) {
-        backToTopButton.classList.add('active');
+        backToTopButton
+          .classList
+          .add('active');
       } else {
-        backToTopButton.classList.remove('active');
+        backToTopButton
+          .classList
+          .remove('active');
       }
     });
     backToTopButton.addEventListener('click', () => {
-      window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: 'smooth'
-      });
+      window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
     });
   }
 
 }
 
-document.addEventListener('DOMContentLoaded', function (event) {
-  const NavApp = new NavApplication();
-});
+document
+  .addEventListener('DOMContentLoaded', function (event) {
+    const NavApp = new NavApplication();
+  });
