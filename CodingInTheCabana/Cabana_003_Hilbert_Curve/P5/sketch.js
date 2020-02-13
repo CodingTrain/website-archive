c9ce7eaaf94f6cc907ed7490d92225ca3e4e6653
Konrad Linkowski <linkowski.konrad97@gmail.com>
2020-02-13 23:40:17 +0100

    Optimization changes in Hilbert

--- CodingInTheCabana/Cabana_003_Hilbert_Curve/P5/sketch.js
+++ CodingInTheCabana/Cabana_003_Hilbert_Curve/P5/sketch.js
@@ -6,21 +6,21 @@
 // https://editor.p5js.org/codingtrain/sketches/LPf9PLmp
 
 const order = 8;
-let N;
-let total;
-
-let path = [];
-
-let counter = 0;
+const path = [];
+const jump = 50;
+let counter = 1;
 
 function setup() {
   createCanvas(512, 512);
   colorMode(HSB, 360, 255, 255);
   background(0);
+  stroke(255);
+  strokeWeight(1);
+  noFill();
 
-  N = int(pow(2, order));
-  total = N * N;
-  let len = width / N;
+  const N = int(pow(2, order));
+  const total = N * N;
+  const len = width / N;
 
   for (let i = 0; i < total; i++) {
     path[i] = hilbert(i);
@@ -30,29 +30,17 @@ function setup() {
 }
 
 function draw() {
-  background(0);
-
-  stroke(255);
-  strokeWeight(1);
-  noFill();
-  //beginShape();
-  for (let i = 1; i < counter; i++) {
-    let h = map(i, 0, path.length, 0, 360);
+  for (let i = counter; i < counter + jump && i < path.length; i++) {
+    const h = map(i, 0, path.length, 0, 360);
     stroke(h, 255, 255);
     line(path[i].x, path[i].y, path[i - 1].x, path[i - 1].y);
   }
-  //endShape();
 
   counter += 50;
   if (counter >= path.length) {
-    counter = 0;
+    counter = 1;
+    background(0);
   }
-
-  // strokeWeight(4);
-  // for (let i = 0; i < path.length; i++) {
-  //  point(path[i].x, path[i].y);
-  //  text(i, path[i].x+5, path[i].y);
-  // }
 }
 
 function hilbert(i) {
@@ -64,14 +52,14 @@ function hilbert(i) {
   ];
 
   let index = i & 3;
-  let v = points[index];
+  const v = points[index];
 
   for (let j = 1; j < order; j++) {
     i = i >>> 2;
     index = i & 3;
-    let len = pow(2, j);
+    const len = pow(2, j);
     if (index == 0) {
-      let temp = v.x;
+      const temp = v.x;
       v.x = v.y;
       v.y = temp;
     } else if (index == 1) {
@@ -80,7 +68,7 @@ function hilbert(i) {
       v.x += len;
       v.y += len;
     } else if (index == 3) {
-      let temp = len - 1 - v.x;
+      const temp = len - 1 - v.x;
       v.x = len - 1 - v.y;
       v.y = temp;
       v.x += len;
