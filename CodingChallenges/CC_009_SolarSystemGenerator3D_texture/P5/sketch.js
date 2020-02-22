5020b9b849e6e12efa9cb7ee3eb4c4ba3f73fbdc
Violet <43045568+violetcraze@users.noreply.github.com>
2020-02-22 16:34:05 -0700

    adding web editor to sketch.js

--- CodingChallenges/CC_009_SolarSystemGenerator3D_texture/P5/sketch.js
+++ CodingChallenges/CC_009_SolarSystemGenerator3D_texture/P5/sketch.js
@@ -2,6 +2,7 @@
 // The Coding Train / Daniel Shiffman
 // https://thecodingtrain.com/CodingChallenges/009-solarsystemgenerator3d-texture.html
 // https://youtu.be/FGAwi7wpU8c
+// https://editor.p5js.org/codingtrain/sketches/SD8a6k6A
 
 // Since PeasyCam is only for Java, this instead uses p5.EasyCam,
 // which is based on PeasyCam but is made for p5.js.
