ac23216e8ec5aa76aee8ad7d684b923c2fed26ee
Gruselhaus <nico@gruselhaus.com>
2020-03-01 00:51:31 +0100

    Welcome p5@1.0.0

--- p5Update.js
+++ p5Update.js
@@ -5,7 +5,8 @@
 const replace = require("replace-in-file");
 
 //Specify the release you want to update to
-const release = "0.10.2";
+const old_release = "0.10.2";
+const release = "1.0.0";
 
 const paths = ["./**/index.html"];
 const ignored_files = ["./_site/**/*.html"];
@@ -13,65 +14,32 @@ const ignored_files = ["./_site/**/*.html"];
 const p5_js = {
   name: "p5.js",
   files: paths,
-  from: new RegExp(
-    `https:\/\/cdn.jsdelivr.net\/npm\/p5@[0-99].[0-99].[0-99]\/lib\/p5.js`
-  ),
+  from: new RegExp(`https://cdn.jsdelivr.net/npm/p5@${old_release}/lib/p5.min.js`),
   to: `https://cdn.jsdelivr.net/npm/p5@${release}/lib/p5.min.js`,
   ignore: ignored_files
 };
 const p5_sound_js = {
   name: "p5.sound.js",
   files: paths,
-  from: new RegExp(
-    `https:\/\/cdn.jsdelivr.net\/npm\/p5@[0-99].[0-99].[0-99]\/lib\/addons\/p5.sound.js`
-  ),
+  from: new RegExp(`https://cdn.jsdelivr.net/npm/p5@${old_release}/lib/addons/p5.sound.min.js`),
   to: `https://cdn.jsdelivr.net/npm/p5@${release}/lib/addons/p5.sound.min.js`,
-  ignore: ignored_files
-};
-const p5_dom_js = {
-  name: "p5.dom.js",
-  files: paths,
-  from: new RegExp(
-    `https:\/\/cdn.jsdelivr.net\/npm\/p5@[0-99].[0-99].[0-99]\/lib\/addons\/p5.dom.js`
-  ),
-  to: ``,
   ignore: ignored_files
 };
 const p5_min_js = {
   name: "p5.min.js",
   files: paths,
-  from: new RegExp(
-    `https:\/\/cdn.jsdelivr.net\/npm\/p5@[0-99].[0-99].[0-99]\/lib\/p5.min.js`
-  ),
+  from: new RegExp(`https://cdn.jsdelivr.net/npm/p5@${old_release}/lib/p5.min.js`),
   to: `https://cdn.jsdelivr.net/npm/p5@${release}/lib/p5.min.js`,
   ignore: ignored_files
 };
 const p5_sound_min_js = {
   name: "p5.sound.min.js",
   files: paths,
-  from: new RegExp(
-    `https:\/\/cdn.jsdelivr.net\/npm\/p5@[0-99].[0-99].[0-99]\/lib\/addons\/p5.sound.min.js`
-  ),
+  from: new RegExp(`https://cdn.jsdelivr.net/npm/p5@${old_release}/lib/addons/p5.sound.min.js`),
   to: `https://cdn.jsdelivr.net/npm/p5@${release}/lib/addons/p5.sound.min.js`,
   ignore: ignored_files
 };
-const p5_dom_min_js = {
-  name: "p5.dom.min.js",
-  files: paths,
-  from: new RegExp(
-    `https:\/\/cdn.jsdelivr.net\/npm\/p5@[0-99].[0-99].[0-99]\/lib\/addons\/p5.dom.min.js`
-  ),
-  to: ``,
-  ignore: ignored_files
-};
-const options = [
-  p5_js,
-  p5_sound_js,
-  p5_dom_js,
-  p5_min_js,
-  p5_sound_min_js,
-  p5_dom_min_js
-];
+const options = [p5_js, p5_sound_js, p5_min_js, p5_sound_min_js];
 
 (async () => {
   for (const option of options) {
