a048bba297393847d679e41afaddf287f42f018d
Frode Austvik <edorfaus@xepher.net>
2020-03-22 13:04:33 +0100

    Change Cubie class to use separate shapes as QUADS isn't implemented.
    This is a workaround for issue p5.js#4401 : in WebGL mode, the QUADS
    mode of beginShape() has not been implemented yet. As a replacement,
    this code now uses a separate shape for each face of the cubie.

--- CodingChallenges/CC_142_Rubiks_Cube_1/P5/Cubie.js
+++ CodingChallenges/CC_142_Rubiks_Cube_1/P5/Cubie.js
@@ -14,49 +14,63 @@ class Cubie {
     strokeWeight(8);
     push();
     translate(this.pos.x, this.pos.y, this.pos.z);
-    beginShape(QUADS);
     const r = this.len / 2;
 
+    // When this was ported, p5.js (version 1.0.0) had not yet
+    // implemented support for beginShape(QUADS) in WEBGL mode.
+    // See: https://github.com/processing/p5.js/issues/4401
+    // So instead, we use separate shapes for each face of the cubie.
+
     // z-fixed
+    beginShape();
     fill(colors[BCK]);
     vertex(-r, -r, -r);
     vertex(r, -r, -r);
     vertex(r, r, -r);
     vertex(-r, r, -r);
+    endShape(CLOSE);
 
+    beginShape();
     fill(colors[FRT]);
     vertex(-r, -r, r);
     vertex(r, -r, r);
     vertex(r, r, r);
     vertex(-r, r, r);
+    endShape(CLOSE);
 
     // y-fixed
+    beginShape();
     fill(colors[DWN]);
     vertex(-r, -r, -r);
     vertex(r, -r, -r);
     vertex(r, -r, r);
     vertex(-r, -r, r);
+    endShape(CLOSE);
 
+    beginShape();
     fill(colors[UPP]);
     vertex(-r, r, -r);
     vertex(r, r, -r);
     vertex(r, r, r);
     vertex(-r, r, r);
+    endShape(CLOSE);
 
     // x-fixed
+    beginShape();
     fill(colors[LFT]);
     vertex(-r, -r, -r);
     vertex(-r, r, -r);
     vertex(-r, r, r);
     vertex(-r, -r, r);
+    endShape(CLOSE);
 
+    beginShape();
     fill(colors[RGT]);
     vertex(r, -r, -r);
     vertex(r, r, -r);
     vertex(r, r, r);
     vertex(r, -r, r);
-
-    endShape();
+    endShape(CLOSE);
 
     //box(this.len);
     pop();
