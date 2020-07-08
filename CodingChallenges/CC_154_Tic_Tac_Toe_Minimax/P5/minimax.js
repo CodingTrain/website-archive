95946b89f37db78a259a4d080e0ba1c26924f83b
Birkan Tuğcu <birkantugcu@gmail.com>
2020-07-07 21:11:10 -0400

    minimizing the function
    I tried that into my project. Less coded version.

--- CodingChallenges/CC_154_Tic_Tac_Toe_Minimax/P5/minimax.js
+++ CodingChallenges/CC_154_Tic_Tac_Toe_Minimax/P5/minimax.js
@@ -33,38 +33,22 @@ let scores = {
 };
 
 function minimax(board, depth, isMaximizing) {
-  let result = checkWinner();
-  if (result !== null) {
-    return scores[result];
-  }
+    let result = checkWinner();
+    if (result !== null) {
+        return scores[result];
+    }
 
-  if (isMaximizing) {
-    let bestScore = -Infinity;
+    let bestScore = isMaximizing ? -Infinity : Infinity;
     for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        // Is the spot available?
-        if (board[i][j] == '') {
-          board[i][j] = ai;
-          let score = minimax(board, depth + 1, false);
-          board[i][j] = '';
-          bestScore = max(score, bestScore);
+        for (let j = 0; j < 3; j++) {
+            // Is the spot available?
+            if (board[i][j] == '') {
+                board[i][j] = isMaximizing ? ai : human;
+                let score = minimax(board, depth + 1, !isMaximizing);
+                board[i][j] = '';
+                bestScore = isMaximizing ? max(score, bestScore) : min(score, bestScore);
+            }
         }
-      }
     }
     return bestScore;
-  } else {
-    let bestScore = Infinity;
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        // Is the spot available?
-        if (board[i][j] == '') {
-          board[i][j] = human;
-          let score = minimax(board, depth + 1, true);
-          board[i][j] = '';
-          bestScore = min(score, bestScore);
-        }
-      }
-    }
-    return bestScore;
-  }
 }
