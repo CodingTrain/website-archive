6437c83acee4e254153b4591983a1b8c581b383f
violet <violet@violetcraze.net>
2020-10-19 09:36:35 -0600

    moving to earlier in the playlist

--- learning/ml5/1.3-object-detection/2_webcam/sketch.js
+++ learning/ml5/1.3-object-detection/2_webcam/sketch.js
@@ -1,17 +1,19 @@
-// ml5.js: Object Detection with COCO-SSD (Image)
+// ml5.js: Object Detection with COCO-SSD (Webcam)
 // The Coding Train / Daniel Shiffman
-// https://thecodingtrain.com/learning/ml5/9.1-object-detection.html
+// https://thecodingtrain.com/learning/ml5/1.3-object-detection.html
 // https://youtu.be/QEzRxnuaZCk
 
 // p5.js Web Editor - Image: https://editor.p5js.org/codingtrain/sketches/ZNQQx2n5o
 // p5.js Web Editor - Webcam: https://editor.p5js.org/codingtrain/sketches/VIYRpcME3
 // p5.js Web Editor - Webcam Persistence: https://editor.p5js.org/codingtrain/sketches/Vt9xeTxWJ
 
-let img;
+// let img;
+let video;
 let detector;
+let detections = [];
 
 function preload() {
-  img = loadImage('dog_cat.jpg');
+  // img = loadImage('dog_cat.jpg');
   detector = ml5.objectDetector('cocossd');
 }
 
@@ -19,9 +21,23 @@ function gotDetections(error, results) {
   if (error) {
     console.error(error);
   }
-  console.log(results);
-  for (let i = 0; i < results.length; i++) {
-    let object = results[i];
+  detections = results;
+  detector.detect(video, gotDetections);
+}
+
+function setup() {
+  createCanvas(640, 480);
+  video = createCapture(VIDEO);
+  video.size(640, 480);
+  video.hide();
+  detector.detect(video, gotDetections);
+}
+
+function draw() {
+  image(video, 0, 0);
+
+  for (let i = 0; i < detections.length; i++) {
+    let object = detections[i];
     stroke(0, 255, 0);
     strokeWeight(4);
     noFill();
@@ -31,11 +47,4 @@ function gotDetections(error, results) {
     textSize(24);
     text(object.label, object.x + 10, object.y + 24);
   }
-}
-
-function setup() {
-  createCanvas(640, 480);
-  // console.log(detector);
-  image(img, 0, 0);
-  detector.detect(img, gotDetections);
 }
