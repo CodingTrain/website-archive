c13f5483b31e336b067fa4b0613aaa65733d3501
violet <violet@violetcraze.net>
2021-02-22 05:46:25 -0700

    Updating page for Code! 2.1 for New Video

--- beginners/p5js/2.1-mousex-mousey/P5/sketch.js
+++ beginners/p5js/2.1-mousex-mousey/P5/sketch.js
@@ -2,22 +2,20 @@
 // Code! Programming with p5.js
 // The Coding Train / Daniel Shiffman
 // https://thecodingtrain.com/beginners/p5js/2.1-mousex-mousey.html
-// https://youtu.be/RnS0YNuLfQQ
-// https://editor.p5js.org/codingtrain/sketches/Nnn1OpIg
+// https://youtu.be/7A5tKW9HGoM
+// https://editor.p5js.org/codingtrain/sketches/IeblvUQrf
 
 function setup() {
-  createCanvas(600, 400);
-  // background
-  background(250, 250, 100);
+  createCanvas(800, 600);
+  background(0);
 }
 
 function draw() {
-  // ellipse
   noStroke();
-  fill(250, 200, 200, 50);
-  ellipse(mouseX, mouseY, 25, 25);
+  fill(255, 50);
+  circle(mouseX, mouseY, 24);
 }
 
 function mousePressed() {
-  background(250, 250, 100);
+  background(0);
 }
