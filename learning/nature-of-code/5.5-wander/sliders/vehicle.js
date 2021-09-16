c0aef999ee68a534c465903217fbacdc1caa68a4
duskvirkus <duskvirkus@protonmail.com>
2021-09-15 22:52:02 -0600

    Adding page for NOC 5.5

---
+++ learning/nature-of-code/5.5-wander/sliders/vehicle.js
@@ -0,0 +1,190 @@
+// Wander (Sliders)
+// The Nature of Code
+// The Coding Train / Daniel Shiffman
+// https://youtu.be/ujsR2vcJlLk
+// https://thecodingtrain.com/learning/nature-of-code/5.5-wander.html
+
+// Main: https://editor.p5js.org/codingtrain/sketches/LVtVlS52Q
+// With Sliders: https://editor.p5js.org/codingtrain/sketches/uxemh7FGc
+// Deleting Positions: https://editor.p5js.org/codingtrain/sketches/EWHjy--Os
+// 3D: https://editor.p5js.org/codingtrain/sketches/t6sFXmVrk
+// Displacement: https://editor.p5js.org/codingtrain/sketches/VdHUvgHkm
+// Perlin Noise: https://editor.p5js.org/codingtrain/sketches/XH2DtikuI
+
+class Vehicle {
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    this.vel = createVector(1, 0);
+    this.acc = createVector(0, 0);
+    this.maxSpeed = 4;
+    this.maxForce = 0.2;
+    this.r = 16;
+
+    this.wanderTheta = PI / 2;
+
+    this.currentPath = [];
+    this.paths = [this.currentPath];
+  }
+
+  wander() {
+    let wanderPoint = this.vel.copy();
+    wanderPoint.setMag(slider1.value());
+    wanderPoint.add(this.pos);
+    fill(255, 0, 0);
+    noStroke();
+    circle(wanderPoint.x, wanderPoint.y, 8);
+
+    let wanderRadius = slider2.value();
+    noFill();
+    stroke(255, 200);
+    circle(wanderPoint.x, wanderPoint.y, wanderRadius * 2);
+    line(this.pos.x, this.pos.y, wanderPoint.x, wanderPoint.y);
+
+    let theta = this.wanderTheta + this.vel.heading();
+
+    let x = wanderRadius * cos(theta);
+    let y = wanderRadius * sin(theta);
+    wanderPoint.add(x, y);
+    fill(0, 255, 0);
+    noStroke();
+    circle(wanderPoint.x, wanderPoint.y, slider3.value() * 100);
+
+    stroke(255, 150);
+    line(this.pos.x, this.pos.y, wanderPoint.x, wanderPoint.y);
+
+    let steer = wanderPoint.sub(this.pos);
+    steer.setMag(this.maxForce);
+    this.applyForce(steer);
+
+    let displaceRange = slider3.value();
+    if (!pause) this.wanderTheta += random(-displaceRange, displaceRange);
+  }
+
+  evade(vehicle) {
+    let pursuit = this.pursue(vehicle);
+    pursuit.mult(-1);
+    return pursuit;
+  }
+
+  pursue(vehicle) {
+    let target = vehicle.pos.copy();
+    let prediction = vehicle.vel.copy();
+    prediction.mult(10);
+    target.add(prediction);
+    fill(0, 255, 0);
+    circle(target.x, target.y, 16);
+    return this.seek(target);
+  }
+
+  arrive(target) {
+    // 2nd argument true enables the arrival behavior
+    return this.seek(target, true);
+  }
+
+  flee(target) {
+    return this.seek(target).mult(-1);
+  }
+
+  seek(target, arrival = false) {
+    let force = p5.Vector.sub(target, this.pos);
+    let desiredSpeed = this.maxSpeed;
+    if (arrival) {
+      let slowRadius = 100;
+      let distance = force.mag();
+      if (distance < slowRadius) {
+        desiredSpeed = map(distance, 0, slowRadius, 0, this.maxSpeed);
+      }
+    }
+    force.setMag(desiredSpeed);
+    force.sub(this.vel);
+    force.limit(this.maxForce);
+    return force;
+  }
+
+  applyForce(force) {
+    this.acc.add(force);
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxSpeed);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+
+    this.currentPath.push(this.pos.copy());
+
+    // Count positions
+    let total = 0;
+    for (let path of this.paths) {
+      total += path.length;
+    }
+
+    if (total > 100) {
+      this.paths[0].shift();
+      if (this.paths[0].length === 0) {
+        this.paths.shift();
+      }
+    }
+  }
+
+  show() {
+    stroke(255);
+    strokeWeight(2);
+    fill(255);
+    push();
+    translate(this.pos.x, this.pos.y);
+    rotate(this.vel.heading());
+    triangle(-this.r, -this.r / 2, -this.r, this.r / 2, this.r, 0);
+    pop();
+
+    for (let path of this.paths) {
+      beginShape();
+      noFill();
+      for (let v of path) {
+        vertex(v.x, v.y);
+      }
+      endShape();
+    }
+  }
+
+  edges() {
+    let hitEdge = false;
+    if (this.pos.x > width + this.r) {
+      this.pos.x = -this.r;
+      hitEdge = true;
+    } else if (this.pos.x < -this.r) {
+      this.pos.x = width + this.r;
+      hitEdge = true;
+    }
+    if (this.pos.y > height + this.r) {
+      this.pos.y = -this.r;
+      hitEdge = true;
+    } else if (this.pos.y < -this.r) {
+      this.pos.y = height + this.r;
+      hitEdge = true;
+    }
+
+    if (hitEdge) {
+      this.currentPath = [];
+      this.paths.push(this.currentPath);
+    }
+  }
+}
+
+class Target extends Vehicle {
+  constructor(x, y) {
+    super(x, y);
+    this.vel = p5.Vector.random2D();
+    this.vel.mult(5);
+  }
+
+  show() {
+    stroke(255);
+    strokeWeight(2);
+    fill("#F063A4");
+    push();
+    translate(this.pos.x, this.pos.y);
+    circle(0, 0, this.r * 2);
+    pop();
+  }
+}
